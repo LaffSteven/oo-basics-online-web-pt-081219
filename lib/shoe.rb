733e7846ba9720a235584a43748717c8d80e3b9b
Steven Laff <steve.laff@gmail.com>
2019-08-27 03:45:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,14 +2,15 @@
 
 class shoe
   
-  attr_accessor :brand, :color, :size, :material, :condition
-  #attr_accessor :color
-  #attr_accessor :size
-  #attr_accessor :material
-  #attr_accessor :condition
+  attr_accessor :brand
+  attr_accessor :color
+  attr_accessor :size
+  attr_accessor :material
+  attr_accessor :condition
   
   def initialize(brand)
     @brand = brand
+    @condition = "new"
   end
   
   def cobble
