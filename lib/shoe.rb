97d925516282687ea948eebf39ac6c0009d83c14
Steven Laff <steve.laff@gmail.com>
2019-08-27 03:43:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,10 +2,11 @@
 
 class shoe
   
-  attr_accessor :brand
-  attr_accessor :color
-  attr_accessor :size
-  attr_accessor :material
+  attr_accessor :brand, :color, :size, :material, :condition
+  #attr_accessor :color
+  #attr_accessor :size
+  #attr_accessor :material
+  #attr_accessor :condition
   
   
   
